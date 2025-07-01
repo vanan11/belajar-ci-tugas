9c09fa0ab2cce623b634122e70c1c94668642be7
Vanan11 <vanaryaputra11@gmail.com>
2025-07-01 22:51:37 +0700

    menambahkan data transaksi pembelian

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -29,12 +29,18 @@ $routes->group('keranjang', ['filter' => 'auth'], function ($routes) {
     $routes->get('clear', 'TransaksiController::cart_clear');
 });
 
+$routes->get('checkout', 'TransaksiController::checkout', ['filter' => 'auth']);
+$routes->post('buy', 'TransaksiController::buy', ['filter' => 'auth']);
+
 $routes->group('kategori', ['filter' => 'auth'], function ($routes) {
     $routes->get('', 'KategoriController::index'); 
     $routes->post('', 'KategoriController::store'); 
     $routes->post('edit/(:num)', 'KategoriController::update/$1'); 
     $routes->get('delete/(:num)', 'KategoriController::delete/$1'); 
 });
+
+$routes->get('get-location', 'TransaksiController::getLocation', ['filter' => 'auth']);
+$routes->get('get-cost', 'TransaksiController::getCost', ['filter' => 'auth']);
 
 
 
